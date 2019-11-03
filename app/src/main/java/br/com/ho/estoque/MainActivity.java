1007eaa85ee4558a21686713acceba67db34b830
henriqueho <henriqueho@gmail.com>
2019-11-03 15:42:29 -0200

    oitavo commit

--- app/src/main/java/br/com/ho/estoque/MainActivity.java
+++ app/src/main/java/br/com/ho/estoque/MainActivity.java
@@ -10,65 +10,75 @@ import android.widget.EditText;
 import br.com.ho.estoque.entidade.Produto;
 import br.com.ho.estoque.repositorio.produto.shared_preference.RepositorioProdutoSharedPreference;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mViewHolder.nome = findViewById(R.id.nomeEdit);
+        this.mViewHolder.quantidade = findViewById(R.id.quantidadeEdit);
+        this.mViewHolder.preco = findViewById(R.id.precoEdit);
+        this.mViewHolder.salvar = findViewById(R.id.salvarButton);
 
-        final EditText nome = (EditText) findViewById(R.id.nomeEdit);
-        final EditText quantidade = (EditText) findViewById(R.id.quantidadeEdit);
-        final EditText preco = (EditText) findViewById(R.id.precoEdit);
+        this.mViewHolder.salvar.setOnClickListener(this);
+    }
 
-        Button salvar = (Button) findViewById(R.id.salvarButton);
-        salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Produto produto = new Produto();
-                produto.nome = nome.getText().toString();
-                produto.quantidade = quantidade.getText().toString();
-                produto.preco = preco.getText().toString();
+    @Override
+    public void onClick(View view){
+        if(view.getId()==R.id.salvarButton){
+            Produto produto = new Produto();
+            produto.nome = this.mViewHolder.nome.getText().toString();
+            produto.quantidade = mViewHolder.quantidade.getText().toString();
+            produto.preco = mViewHolder.preco.getText().toString();
 
-                boolean flag = false;
-                boolean flag1 = false;
-                boolean flag2 = false;
-                boolean flag3 = false;
+            boolean flag = false;
+            boolean flag1 = false;
+            boolean flag2 = false;
+            boolean flag3 = false;
 
-                if(produto.nome.isEmpty()) {
-                    nome.setError("o nome do produto se encontra vazio");
-                    flag1 = false;
-                } else {
-                    nome.setError(null);
-                    flag1 = true;
-                }
-
-                if (produto.quantidade.isEmpty()) {
-                    quantidade.setError("a quantidade se encontra vazia");
-                    flag2 = false;
-                } else {
-                    quantidade.setError(null);
-                    flag2 = true;
-                }
-
-                if (produto.preco.isEmpty()) {
-                    preco.setError("o preço se encotra vazio");
-                    flag3 = false;
-                } else {
-                    preco.setError(null);
-                    flag3 = true;
-                }
-
-                flag = flag1&&flag2&&flag3;
-
-                if (flag == true) {
-                    RepositorioProdutoSharedPreference repositorioProdutoSharedPreference = new RepositorioProdutoSharedPreference(MainActivity.this.getApplicationContext());
-                    repositorioProdutoSharedPreference.criaProduto(produto);
-
-                    Intent intent = new Intent(MainActivity.this, salvarActivity.class);
-                    MainActivity.this.startActivity(intent);
-                }
+            if(produto.nome.isEmpty()) {
+                this.mViewHolder.nome.setError("o nome do produto se encontra vazio");
+                flag1 = false;
+            } else {
+                mViewHolder.nome.setError(null);
+                flag1 = true;
             }
-        });
+
+            if (produto.quantidade.isEmpty()) {
+                mViewHolder.quantidade.setError("a quantidade se encontra vazia");
+                flag2 = false;
+            } else {
+                mViewHolder.quantidade.setError(null);
+                flag2 = true;
+            }
+
+            if (produto.preco.isEmpty()) {
+                mViewHolder.preco.setError("o preço se encotra vazio");
+                flag3 = false;
+            } else {
+                mViewHolder.preco.setError(null);
+                flag3 = true;
+            }
+
+            flag = flag1&&flag2&&flag3;
+
+            if (flag == true) {
+                RepositorioProdutoSharedPreference repositorioProdutoSharedPreference = new RepositorioProdutoSharedPreference(MainActivity.this.getApplicationContext());
+                repositorioProdutoSharedPreference.criaProduto(produto);
+
+                Intent intent = new Intent(MainActivity.this, salvarActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        }
+    }
+
+    private static class ViewHolder {
+        EditText nome;
+        EditText quantidade;
+        EditText preco;
+        Button salvar;
     }
 }
